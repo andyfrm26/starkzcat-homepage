d498547fc9223c193d8f603bd8b56a6810e0920b
Andy Firmansyah <andyfirmansyah2667@gmail.com>
2024-06-13 07:22:22 +0700

    fix: missing meta data

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,3 +1,4 @@
+import Head from "next/head"
 import { JetBrains_Mono } from "next/font/google"
 import "./globals.css"
 import Header from "@/components/Header"
@@ -19,9 +20,23 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-      </head>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="Andy's homepage" />
+        <meta name="author" content="Andy Firmansyah" />
+        <meta name="author" content="starkzcat" />
+        <link rel="apple-touch-icon" href="apple-touch-icon.png" />
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+        <meta name="twitter:title" content="Andy Firmansyah" />
+        <meta name="twitter:card" content="summary_large_image" />
+        {/* <meta name="twitter:site" content="@craftzdog" /> */}
+        {/* <meta name="twitter:creator" content="@craftzdog" /> */}
+        <meta name="twitter:image" content="/card.png" />
+        <meta property="og:site_name" content="Andy Firmansyah" />
+        <meta name="og:title" content="Andy Firmansyah" />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="/card.png" />
+      </Head>
       <body className={jetbrainsMono.variable}>
         <Header />
         <StairTransition />
